2524334165ddbf479c07b7311db9130a618ab1ec
qinhuangsheng <qinhuangsheng@china-leadpeak.com>
2021-01-05 11:02:37 +0800

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
+const plugins = []
+
+// 按需引入ant-design-vue组件，会自动引入组件对应的css文件
+plugins.push(['import', {
+  'libraryName': 'ant-design-vue',
+  'libraryDirectory': 'es',
+  'style': true // `style: true` 会加载 less 文件
+}])
+
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    '@vue/cli-plugin-babel/preset',
+  ],
+  plugins
 }
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+// import { Table } from 'ant-design-vue'
 
 Vue.config.productionTip = false
+
+// Vue.use(Table)
 
 new Vue({
   render: h => h(App),
